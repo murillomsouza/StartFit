63286afb39ee0e4079dbd33dcf913b9003485058
murillomsouza <murillosouza997@gmail.com>
2025-11-24 09:59:53 -0300

    feat: create initial login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/styles_login.cs">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div>
+
+    </div>
+    <aside>
+        <div>
+        <h1>Teste123</h1>
+        </div>
+        
+    </aside>
+    <img src="img/foto_login.png" alt="Foto do Login">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js">    
+</body>
+</html>
